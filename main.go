259359fe4ecb0b1e5b8a1933c47c43dcdc26cb58
Alejandro Loaiza Arango <alloaiza@bancolombia.com.co>
2018-03-24 15:06:05 -0500

    [Add] logic to fetch from public Api

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import "net"
+import "net/http"
 import "fmt"
 import "bufio"
 import "io"
+import "io/ioutil"
 import "strings"
 import "time"
+//import "os"
 //import "strconv"
+
+
 
 func main() {
 
+	/*var attr os.ProcAttr 
+	attr.Sys.HideWindow = true
+	p, err := os.StartProcess("whatever", nil, &attr)
+	*/
 	for {
   		conn, err := net.Dial("tcp", "chat.freenode.net:6667")
 
@@ -27,6 +36,7 @@ func main() {
 	    	message, err := MyReader.ReadString('\n')
 			// atomixxx: To handle if connection is closed, and terminate the program.
 			if err != nil {
+				fmt.Println(err)
 				if io.EOF == err {
 					conn.Close()
 					fmt.Println("server closed connection")
@@ -43,22 +53,33 @@ func main() {
 			// atomixxx: Logic to detect messages, BOT logic should go inside this
 			if len(text) >= 4 && text[1] == "PRIVMSG" {
 				var response string
+				var repeat bool = true
+				var respondTo string
 				// logic to differ if message is channel or private from user
 				if text[2][0:1] == "#" {
 					fmt.Println("Message detected from channel")
 					// logic to respond the same thing to a channel / repeater BOT
-					response = "PRIVMSG " + text[2] + " " + strings.Join(text[3:]," ") 
+					respondTo =  text[2]
 				} else {
 					fmt.Println("Message detected from user")
 					userto := strings.Split(text[0],"!")
+					respondTo = userto[0][1:]
 					// logic to respond the same thing to a user / repeater BOT
-					response = "PRIVMSG " + userto[0][1:] + " " + strings.Join(text[3:]," ") 
 				}
-				fmt.Fprintln(conn,response)
-				fmt.Println("<<"+response)
-				
+				if text[3] == ":!command:" {
+					repeat = false
+					commandresponse := processCommand(text[4:])
+					response = "PRIVMSG " + respondTo + " :" + commandresponse 
+					fmt.Fprintln(conn,response)
+					fmt.Println("<<"+response)
+				} 
+				if repeat == true {
+					response = "PRIVMSG " + respondTo + " " + strings.Join(text[3:]," ") 
+					fmt.Fprintln(conn,response)
+					fmt.Println("<<"+response)
+				}
 			}
-			// atomixxx: Ping/Pong handler to avoid timeout from the irc server
+			// atomixxx: Ping/Pong handler to avoid timeout disconnect from the irc server
 			if len(text) == 2 && text[0] == "PING"  {
 				pong := "PONG "+text[1]
 				fmt.Fprintln(conn, pong)
@@ -69,4 +90,30 @@ func main() {
 		// atomixxx: If connection is closed, will try to reconnect after 2 seconds
 		time.Sleep(2000 * time.Millisecond)
 	}
+
+}
+func processCommand(command[] string) string {
+		var bodyString string
+		fmt.Println("Command request inside process: " +command[0] +"|" )
+		if command[0] == "api" {
+			fmt.Println("api!")
+			req, err := http.NewRequest("GET", "http://47.88.174.2:3000/api/transactions/71527525", nil)
+			if err != nil {
+				fmt.Println("Error in newRequest: ", err)
+				
+			}
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println("Error in Response: ", err)
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode == http.StatusOK {
+    			bodyBytes, _ := ioutil.ReadAll(resp.Body)
+    			bodyString = string(bodyBytes)
+				fmt.Println(bodyString)
+			}
+		}
+		
+		return bodyString
 }
