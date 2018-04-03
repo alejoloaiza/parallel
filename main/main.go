954757fc20c42fd7d21111a844b1f5aa37afddfa
atomixxx <alejo.loaiza@gmail.com>
2018-04-03 16:55:38 -0500

    [Add] Adding logic to classify data

--- main/main.go
+++ main/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"parallel/irc"
+	"fmt"
 	"os"
-	"parallel/db"
 	"parallel/data"
+	"parallel/db"
+	"parallel/irc"
 )
 
 func main() {
-	
+
 	arg := "../config/config.json"
-	if len(os.Args) > 1 { arg = os.Args[1] }
+	if len(os.Args) > 1 {
+		arg = os.Args[1]
+	}
 
 	//collyclient.Initcollyclient_Agency1()
-	//db.DBConnectPostgres(arg)
+	db.DBConnectPostgres(arg)
 	db.DBConnectRedis()
+	data.FillRawAssetsArray()
 	go irc.StartIRCprocess(arg)
-	data.GetAllKeys()
-
+	data.PrintAssetsArray()
+	data.AssetTypeClassifier()
+	data.AssetBusinessClassifier()
+	fmt.Println("=======================================")
+	data.PrintAssetsArray()
 }
